c48c2cee934a32c4a161a31e53737c0fc325743b
mohammadjavad rabiei <mj.rabiei98@gmail.com>
2024-04-28 14:09:01 +0330

    final version

--- pattern_finder.vhdl
+++ pattern_finder.vhdl
@@ -4,30 +4,22 @@ PACKAGE generic_array_type IS
     TYPE kernel_array IS ARRAY (0 TO 2, 0 TO 8) OF STD_LOGIC_VECTOR(7 DOWNTO 0);
     TYPE bias_array IS ARRAY (0 TO 2) OF STD_LOGIC_VECTOR(7 DOWNTO 0);
 END PACKAGE generic_array_type;
+
+
+
+
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 USE work.generic_array_type.ALL;
+
 ENTITY patter_finder IS
     GENERIC (
         data_width : INTEGER := 8;
         number_of_conv : INTEGER := 3;
         kernel_size : INTEGER := 3;
-        kernels_array : kernel_array :=
-        ((
-        "00000000", "00000001", "00000000",
-        "00000001", "00000001", "00000001",
-        "00000000", "00000001", "00000000"
-        ), (
-        "00000001", "00000001", "00000001",
-        "00000001", "00000000", "00000000",
-        "00000001", "00000001", "00000001"
-        ), (
-        "00000001", "00000001", "00000001",
-        "00000000", "00000001", "00000000",
-        "00000000", "00000001", "00000000"
-        ));
-        bias_arrays : bias_array := ("11111111", "11111110", "11111110");
-        image_size : STD_LOGIC_VECTOR(7 DOWNTO 0) := "00000100"
+        kernels_array : kernel_array;
+        bias_arrays : bias_array;
+        image_size : STD_LOGIC_VECTOR(7 DOWNTO 0)
     );
     PORT (
         start, clk, rst, write_ram : IN STD_LOGIC;
@@ -48,29 +40,6 @@ ARCHITECTURE configurable OF patter_finder IS
     SIGNAL relus_out : relu_out;
     TYPE maxpool_out IS ARRAY (0 TO number_of_conv - 1) OF STD_LOGIC_VECTOR(DATA_WIDTH - 1 DOWNTO 0);
     SIGNAL maxpools_out : maxpool_out;
-    -- COMPONENT conv
-    --     GENERIC (
-    --         bias_value : STD_LOGIC_VECTOR(7 DOWNTO 0) := "00000000";
-    --         image_size : STD_LOGIC_VECTOR(7 DOWNTO 0) := "00000100";
-    --         data_width : INTEGER := 8;
-    --         kernet_1 : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
-    --         kernet_2 : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
-    --         kernet_3 : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
-    --         kernet_4 : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
-    --         kernet_5 : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
-    --         kernet_6 : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
-    --         kernet_7 : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
-    --         kernet_8 : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
-    --         kernet_9 : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0')
-    --     );
-    --     PORT (
-    --         SIGNAL clk, rst, start : IN STD_LOGIC;
-    --         SIGNAL data_in : IN STD_LOGIC_VECTOR(data_width - 1 DOWNTO 0);
-    --         SIGNAL data_out1, data_out2, data_out3, data_out4 : OUT STD_LOGIC_VECTOR(data_width - 1 DOWNTO 0);
-    --         SIGNAL done : OUT STD_LOGIC;
-    --         SIGNAL address_out : OUT STD_LOGIC_VECTOR(data_width - 1 DOWNTO 0)
-    --     );
-    -- END COMPONENT conv;
 BEGIN
     ram1 : ENTITY work.ram(behavioral)
         GENERIC MAP(8, 16)
@@ -81,10 +50,9 @@ BEGIN
             write_en, read_en,
             ram_data_out
         );
-    -- component
+
     gen_circuit : FOR i IN 0 TO number_of_conv - 1 GENERATE
     BEGIN
-        -- conv_i : conv
         conv_i : ENTITY work.convolution(modular)
         GENERIC MAP(
             bias_arrays(i), image_size, 8, kernels_array(i, 0), kernels_array(i, 1), kernels_array(i, 2),
@@ -114,6 +82,35 @@ BEGIN
         );
 
 END configurable; -- behavioral
+
+
+
+-- COMPONENT conv
+    --     GENERIC (
+    --         bias_value : STD_LOGIC_VECTOR(7 DOWNTO 0) := "00000000";
+    --         image_size : STD_LOGIC_VECTOR(7 DOWNTO 0) := "00000100";
+    --         data_width : INTEGER := 8;
+    --         kernet_1 : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
+    --         kernet_2 : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
+    --         kernet_3 : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
+    --         kernet_4 : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
+    --         kernet_5 : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
+    --         kernet_6 : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
+    --         kernet_7 : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
+    --         kernet_8 : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
+    --         kernet_9 : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0')
+    --     );
+    --     PORT (
+    --         SIGNAL clk, rst, start : IN STD_LOGIC;
+    --         SIGNAL data_in : IN STD_LOGIC_VECTOR(data_width - 1 DOWNTO 0);
+    --         SIGNAL data_out1, data_out2, data_out3, data_out4 : OUT STD_LOGIC_VECTOR(data_width - 1 DOWNTO 0);
+    --         SIGNAL done : OUT STD_LOGIC;
+    --         SIGNAL address_out : OUT STD_LOGIC_VECTOR(data_width - 1 DOWNTO 0)
+    --     );
+    -- END COMPONENT conv;
+
+
+    -- after efingin component in arxhitucture body then we can configure the elements in below
 
 
 
